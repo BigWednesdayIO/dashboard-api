21938509fcb6fed0fd61d28b772dbabbd2be174c
toboid <toboid@users.noreply.github.com>
2015-10-26 17:22:07 +0000

    Add company to user

--- spec/users.js
+++ spec/users.js
@@ -5,20 +5,46 @@ const specRequest = require('./spec_request');
 
 describe('/users', () => {
   describe('post', () => {
-    it('creates a user', () => {
-      return specRequest({
-        url: '/users',
-        method: 'POST',
-        payload: {email: 'test@bigwednesday.io'}
-      })
-      .then(response => {
-        expect(response.statusCode).to.equal(201);
-        expect(response.headers.location).to.equal(`/users/${response.result.id}`);
+    describe('created user', () => {
+      let createUserResponse;
+
+      beforeEach(() => {
+        createUserResponse = undefined;
+        return specRequest({
+          url: '/users',
+          method: 'POST',
+          payload: {
+            email: 'test@bigwednesday.io',
+            company: 'A company'
+          }
+        })
+        .then(response => {
+          if (response.statusCode !== 201) {
+            return console.error(response.result);
+          }
+          createUserResponse = response;
+        });
+      });
+
+      it('returns http 201', () => {
+        expect(createUserResponse.statusCode).to.equal(201);
+      });
+
+      it('provides created resource location', () => {
+        expect(createUserResponse.headers.location).to.equal(`/users/${createUserResponse.result.id}`);
+      });
+
+      it('has email address', () => {
+        expect(createUserResponse.result.email).to.equal('test@bigwednesday.io');
+      });
+
+      it('has company', () => {
+        expect(createUserResponse.result.company).to.equal('A company');
       });
     });
 
     describe('validation', () => {
-      it('requires an email address', () => {
+      it('requires email address', () => {
         return specRequest({
           url: '/users',
           method: 'POST',
@@ -27,6 +53,30 @@ describe('/users', () => {
         .then(response => {
           expect(response.statusCode).to.equal(400);
           expect(response.result.message).to.equal('child "email" fails because ["email" is required]');
+        });
+      });
+
+      it('validates email address format', () => {
+        return specRequest({
+          url: '/users',
+          method: 'POST',
+          payload: {email: 'bigwednesday.io'}
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(400);
+          expect(response.result.message).to.equal('child "email" fails because ["email" must be a valid email]');
+        });
+      });
+
+      it('validates company is string', () => {
+        return specRequest({
+          url: '/users',
+          method: 'POST',
+          payload: {email: 'test@bigwednesday.io', company: 1}
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(400);
+          expect(response.result.message).to.equal('child "company" fails because ["company" must be a string]');
         });
       });
 
