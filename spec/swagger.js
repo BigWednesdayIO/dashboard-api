3c36bfd53df6a4819f4f8deec091aa1647110d30
toboid <toboid@users.noreply.github.com>
2015-10-28 16:06:55 +0000

    Follow standard describe structure

--- spec/swagger.js
+++ spec/swagger.js
@@ -4,32 +4,34 @@ const swaggerSpec = require('swagger-tools').specs.v2;
 const expect = require('chai').expect;
 
 describe('/swagger', () => {
-  let swagger;
+  describe('post', () => {
+    let swagger;
 
-  before(done => {
-    require('../lib/server')((err, server) => {
-      if (err) {
-        return console.error(err);
-      }
+    before(done => {
+      require('../lib/server')((err, server) => {
+        if (err) {
+          return console.error(err);
+        }
 
-      server.inject({url: '/swagger', method: 'GET'}, response => {
-        swagger = JSON.parse(response.payload);
-        done();
+        server.inject({url: '/swagger', method: 'GET'}, response => {
+          swagger = JSON.parse(response.payload);
+          done();
+        });
       });
     });
-  });
 
-  it('generates a valid swagger schema', () => {
-    swaggerSpec.validate(swagger, (err, result) => {
-      if (result && result.errors) {
-        console.error(result.errors);
-      }
-      if (result && result.warnings) {
-        console.error(result.warnings);
-      }
+    it('generates a valid swagger schema', () => {
+      swaggerSpec.validate(swagger, (err, result) => {
+        if (result && result.errors) {
+          console.error(result.errors);
+        }
+        if (result && result.warnings) {
+          console.error(result.warnings);
+        }
 
-      expect(err).to.not.be.ok;
-      expect(result).to.equal(undefined);
+        expect(err).to.not.be.ok;
+        expect(result).to.equal(undefined);
+      });
     });
   });
 });
