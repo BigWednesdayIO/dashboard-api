6d2a08aaca645d358916109bd69b17c10830daa0
toboid <toboid@users.noreply.github.com>
2015-10-29 11:34:39 +0000

    Force email address to be verified when authenticating. This is required because we allow logging in through Auth0 via social login or username and password and we need to treat those logins as a single account. If a user logged via social then an attacker could potentially access their account if they could signup via username and password without having to verify the email address. Providing feedback for these 2 authorization issues because they do not reveal whether the account exists already.

--- spec/routes/authenticate.js
+++ spec/routes/authenticate.js
@@ -9,19 +9,44 @@ describe('/authenticate', () => {
   describe('post', () => {
     describe('jwt authentication', () => {
       it('requires an email address', () => {
-        const token = tokenBuilder({});
+        const token = tokenBuilder({email_verified: true});
         return specRequest({
           url: `/authenticate?token=${token}`,
           method: 'POST'
         })
         .then(response => {
           expect(response.statusCode).to.equal(401);
+          expect(response.result.message).to.equal('No email address');
+        });
+      });
+
+      it('requires email_verified', () => {
+        const token = tokenBuilder({email: 'unverified_user@bigwednesday.io'});
+        return specRequest({
+          url: `/authenticate?token=${token}`,
+          method: 'POST'
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(401);
+          expect(response.result.message).to.equal('Email address not verified');
+        });
+      });
+
+      it('requires email_verified to be true', () => {
+        const token = tokenBuilder({email: 'unverified_user@bigwednesday.io', email_verified: 'yes'});
+        return specRequest({
+          url: `/authenticate?token=${token}`,
+          method: 'POST'
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(401);
+          expect(response.result.message).to.equal('Email address not verified');
         });
       });
     });
 
     describe('new user', () => {
-      const token = tokenBuilder({email: 'test@bigwednesday.io'});
+      const token = tokenBuilder({email: 'new_user@bigwednesday.io', email_verified: true});
 
       let newUserResponse;
 
@@ -49,7 +74,7 @@ describe('/authenticate', () => {
       });
 
       it('persists user', () => {
-        const createdUser = userDb.findByEmail('test@bigwednesday.io');
+        const createdUser = userDb.findByEmail('new_user@bigwednesday.io');
         expect(createdUser.id).to.equal(newUserResponse.result.id);
       });
     });
